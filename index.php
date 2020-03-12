cc9e1e3ea68e46a30afc09e6cd74515f1be20c24
dan9l10 <gagner1322@gmail.com>
2020-03-12 18:03:30 +0300

    changed

--- index.php
+++ index.php
@@ -44,6 +44,7 @@ and open the template in the editor.
        // print "<br>$row[0] $row[1] $row[2] $row[3] $row[4] $row[2] ";
        // }
        //     echo 'hello';
+        //dfsdsdsddsdprivet
         ?>
     </body>
 </html>
