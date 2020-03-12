121ee5b26c5d74a7ff063ebda1544081daff7955
Gj1337 <56798681+Gj1337@users.noreply.github.com>
2020-03-12 21:46:39 +0200

    Update reg.php
    Добавленны отдельные поля для ФИО,  переименнованы загаловки на русский язык, поле для ввода даты поменяно с текста на календарик.

--- php/reg.php
+++ php/reg.php
@@ -15,18 +15,22 @@ include('connect.php');
     </head>
     <body>
         <form action="check.php" method="POST" enctype="multipart/form-data"> 
-                <h1>Registration</h1>
-                <label>Login</label>
+                <h1>Регистрация</h1>
+                <label>Логин</label>
                 <input type="text" name="login" class="form-control" placeholder="Введите логин">
-                <label>Password</label>
+                <label>Пароль</label>
                 <input type="password" name="password" class="form-control" placeholder="Введите пароь">
-                <label>Name</label>
-                <input type="text" name="name" class="form-control" placeholder="Введите name">
+                <label>Имя</label>
+                <input type="text" name="FirstName" class="form-control" placeholder="Введите name">
+                <label>Фамилия</label>
+                <input type="text" name="LastName" class="form-control" placeholder="Введите name">
+                <label>Отчество</label>
+                <input type="text" name="MiddleName" class="form-control" placeholder="Введите name">
                 <label>Age</label>
-                <input type="text" name="age" class="form-control" placeholder="Введите age">
+                <input type="date" name="age" class="form-control" placeholder="Введите age"  value="2000-01-01">
                 <label>Email</label>
                 <input type="email" name="email" class="form-control" placeholder="Введите email">
-                <label>Image</label>
+                <label>Аватар</label>
                 <input type="file" name="avatar">
                 <button type="submit" class="btn-reg">Register</button><br>
                 <!--<input type="submit" name="OK" class="form-control" value="Зарегестрироваться">-->
