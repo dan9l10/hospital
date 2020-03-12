7648bd18a7170d467bace610b013433f419a8447
Gj1337 <56798681+Gj1337@users.noreply.github.com>
2020-03-12 21:48:38 +0200

    Update auth.php
    В $_SESSION[] добавлены доп поля с ФИО

--- php/auth.php
+++ php/auth.php
@@ -5,22 +5,27 @@ $login = filter_var(trim($_POST['login']),FILTER_SANITIZE_STRING);
 $pass = filter_var(trim($_POST['password']),FILTER_SANITIZE_STRING);
 
 $pass = md5($pass."gyewid143yfdhgwe13");
-//$dbh->query("INSERT INTO `users`(`name`,`login`,`password`,`email`,`status`) VALUES ('$name','$login','$pass','$email','user')");
 $result = $dbh-> query("select * from `users` where `login` = '$login' and `password`= '$pass'");
 $user = $result->fetch(PDO::FETCH_ASSOC);
-if(empty($user)){
+
+if(empty($user))
+{
     $_SESSION['msg']='User not found';
     header('Location: ../index.php');
     exit();
 }
+
  $_SESSION['usr'] = [
-     "name" => $user['name'],
+     "first_name" => $user['first_name'],
+     "last_name" => $user['last_name'],
+     "middle_name" => $user['middle_name'],
      "email" => $user['email'],
      "avatar" => $user['avatar']
+
  ];
-//print_r($user);
-//exit();
-header('Location: ../profile.php');
+
+
+header('Location: ../Patient.php');
 ?>
 
 
