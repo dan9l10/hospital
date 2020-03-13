a148314bfec4e8c8c9395ab6ed00b289400ffb7c
dan9l10 <gagner1322@gmail.com>
2020-03-13 15:54:18 +0300

    upd db

--- hospital.sql
+++ hospital.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Мар 12 2020 г., 20:50
+-- Время создания: Мар 13 2020 г., 13:53
 -- Версия сервера: 10.4.11-MariaDB
--- Версия PHP: 7.4.3
+-- Версия PHP: 7.4.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -39,7 +39,10 @@ CREATE TABLE `patient` (
 --
 
 INSERT INTO `patient` (`id`, `FilePath`, `Fluorography`) VALUES
-(26, 'data/Сверидов Антон Александрович .txt', NULL);
+(26, 'data/Сверидов Антон Александрович .txt', NULL),
+(27, 'data/krasnyi Danylo Aleksando .txt', NULL),
+(28, 'data/krasnyi danylo alexandr .txt', NULL),
+(29, 'data/user user user .txt', NULL);
 
 -- --------------------------------------------------------
 
@@ -65,7 +68,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `login`, `password`, `first_name`, `middle_name`, `last_name`, `DOB`, `email`, `status`, `avatar`) VALUES
-(26, 'antoha228', 'b245dcd110a49b3e7ab5b35ae977388e', 'Антон', 'Александрович', 'Сверидов', '2000-01-01', 'antoha228@gmail.com', 'user', 'img/1584041463');
+(26, 'antoha228', 'b245dcd110a49b3e7ab5b35ae977388e', 'Антон', 'Александрович', 'Сверидов', '2000-01-01', 'antoha228@gmail.com', 'user', 'img/1584041463'),
+(27, 'dan9l10', '729144f3edb69a6dc7dcea5746f96b9d', 'admin', 'adminovich', 'adminskiy', '1998-08-28', 'gagner1322@gmail.com', 'admin', 'img/1584047794'),
+(28, 'dan9l1010', '729144f3edb69a6dc7dcea5746f96b9d', 'doctor', 'doctorovich', 'antonov', '2000-01-01', 'gag@gmail.com', 'doctor', 'img/1584102197'),
+(29, 'dan9l101010', '729144f3edb69a6dc7dcea5746f96b9d', 'user', 'user', 'user', '2000-01-01', 'fgfdf@gmail.com', 'user', 'img/1584103595favicon.ico');
 
 --
 -- Индексы сохранённых таблиц
@@ -91,7 +97,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
