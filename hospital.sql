c40d2c88b2c670cba2c0e6280aa5561d1261403f
dan9l10 <gagner1322@gmail.com>
2020-03-16 03:01:05 +0200

    fix bugs & update db

--- hospital.sql
+++ hospital.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Мар 13 2020 г., 18:18
--- Версия сервера: 10.4.11-MariaDB
--- Версия PHP: 7.4.2
+-- Время создания: Мар 16 2020 г., 02:00
+-- Версия сервера: 10.1.38-MariaDB
+-- Версия PHP: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,24 +25,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `patient`
+-- Структура таблицы `doctors`
 --
 
-CREATE TABLE `patient` (
-  `id` int(11) DEFAULT NULL,
-  `FilePath` text DEFAULT NULL,
-  `Fluorography` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `doctors` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `surname` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `middle_name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `specialization` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `login` varchar(1000) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(1000) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Дамп данных таблицы `patient`
+-- Дамп данных таблицы `doctors`
 --
 
-INSERT INTO `patient` (`id`, `FilePath`, `Fluorography`) VALUES
-(26, 'data/Сверидов Антон Александрович .txt', NULL),
-(27, 'data/krasnyi Danylo Aleksando .txt', NULL),
-(28, 'data/krasnyi danylo alexandr .txt', NULL),
-(29, 'data/user user user .txt', NULL);
+INSERT INTO `doctors` (`id`, `name`, `surname`, `middle_name`, `specialization`, `email`, `login`, `password`) VALUES
+(1, 'table', 'antonov', 'doctorovich', 'LOR', 'gag@gmail.com', 'dan9l1010', '729144f3edb69a6dc7dcea5746f96b9d');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `schedule`
+--
+
+CREATE TABLE `schedule` (
+  `month` int(11) NOT NULL,
+  `day` int(11) DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `id_doctor` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -54,13 +70,13 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `login` text COLLATE utf8_unicode_ci NOT NULL,
   `password` text COLLATE utf8_unicode_ci NOT NULL,
-  `first_name` text COLLATE utf8_unicode_ci DEFAULT NULL,
-  `middle_name` text COLLATE utf8_unicode_ci DEFAULT NULL,
-  `last_name` text COLLATE utf8_unicode_ci DEFAULT NULL,
-  `DOB` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  `first_name` text COLLATE utf8_unicode_ci,
+  `middle_name` text COLLATE utf8_unicode_ci,
+  `last_name` text COLLATE utf8_unicode_ci,
+  `DOB` text COLLATE utf8_unicode_ci,
   `email` text COLLATE utf8_unicode_ci NOT NULL,
-  `status` text COLLATE utf8_unicode_ci DEFAULT NULL,
-  `avatar` text COLLATE utf8_unicode_ci DEFAULT NULL
+  `status` text COLLATE utf8_unicode_ci,
+  `avatar` text COLLATE utf8_unicode_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -70,7 +86,6 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `login`, `password`, `first_name`, `middle_name`, `last_name`, `DOB`, `email`, `status`, `avatar`) VALUES
 (26, 'antoha228', 'b245dcd110a49b3e7ab5b35ae977388e', 'Антон', 'Александрович', 'Сверидов', '2000-01-01', 'antoha228@gmail.com', 'user', 'img/1584041463'),
 (27, 'dan9l10', '729144f3edb69a6dc7dcea5746f96b9d', 'admin', 'adminovich', 'adminskiy', '1998-08-28', '434@gmail.com', 'admin', 'img/1584047794'),
-(28, 'dan9l1010', '729144f3edb69a6dc7dcea5746f96b9d', 'doctor', 'doctorovich', 'antonov', '2000-01-01', 'gag@gmail.com', 'doctor', 'img/1584102197'),
 (29, 'dan9l101010', '729144f3edb69a6dc7dcea5746f96b9d', 'user', 'user', 'user', '2000-01-01', 'fgfdf@gmail.com', 'user', 'img/1584103595favicon.ico');
 
 --
@@ -78,10 +93,18 @@ INSERT INTO `users` (`id`, `login`, `password`, `first_name`, `middle_name`, `la
 --
 
 --
--- Индексы таблицы `patient`
+-- Индексы таблицы `doctors`
 --
-ALTER TABLE `patient`
-  ADD KEY `id` (`id`);
+ALTER TABLE `doctors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `schedule`
+--
+ALTER TABLE `schedule`
+  ADD PRIMARY KEY (`month`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_doctor` (`id_doctor`);
 
 --
 -- Индексы таблицы `users`
@@ -94,20 +117,27 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `doctors`
+--
+ALTER TABLE `doctors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
 
 --
--- Ограничения внешнего ключа таблицы `patient`
+-- Ограничения внешнего ключа таблицы `schedule`
 --
-ALTER TABLE `patient`
-  ADD CONSTRAINT `patient_ibfk_1` FOREIGN KEY (`id`) REFERENCES `users` (`id`);
+ALTER TABLE `schedule`
+  ADD CONSTRAINT `schedule_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `schedule_ibfk_2` FOREIGN KEY (`id_doctor`) REFERENCES `doctors` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
